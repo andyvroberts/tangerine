5cef39c071c3affcb2f8c5af7edc02817070cb8f
Andy.R <avroberts@outlook.com>
2024-05-27 07:31:07 +0100

    return json from functions

--- ingest/BmrsRetriever/BmrsData.cs
+++ ingest/BmrsRetriever/BmrsData.cs
@@ -1,15 +1,16 @@
-using System;
+using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
 
 namespace BmrsRetriever
 {
     public class BmrsData
     {
         [Function("SystemPrices")]
-        public static async Task SystemPrices(
+        public static async Task<IActionResult> SystemPrices(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = "bmrs/price/{year}/{month}/{day}")] HttpRequestData req,
             string year,
             string month,
@@ -23,21 +24,19 @@ namespace BmrsRetriever
             string? apiKey = Environment.GetEnvironmentVariable("BmrsApiKey");
             _logger.LogInformation("Settlement Date = {}", urlDate);
 
-            if (apiKey != null)
-            {
-                var prices = await ImbalanceRequests.BmrsPriceRequestAsync(apiKey, urlDate);
-                
-                var returns = JsonSerializer.Serialize(prices.Select(Mapper.FromXmlToImbalancePrice));
+            if (apiKey == null)
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
 
-                _logger.LogInformation("Found prices\n {}", returns);
-            }
-            else 
-                _logger.LogInformation("No prices found.");
+            var prices = await ImbalanceRequests.BmrsPriceRequestAsync(apiKey, urlDate);    
+            var returns = JsonSerializer.Serialize(prices.Select(Mapper.FromXmlToImbalancePrice));
 
+            return returns.Length > 0 ? 
+                new OkObjectResult(returns) :
+                new NotFoundObjectResult(urlDate);
         }
 
         [Function("SystemVolumes")]
-        public static async Task SystemVolumes(
+        public static async Task<IActionResult> SystemVolumes(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = "bmrs/volume/{year}/{month}/{day}")] HttpRequestData req,
             string year,
             string month,
@@ -51,17 +50,16 @@ namespace BmrsRetriever
             string? apiKey = Environment.GetEnvironmentVariable("BmrsApiKey");
             _logger.LogInformation("Settlement Date = {}", urlDate);
 
-            if (apiKey != null)
-            {
-                var volumes = await ImbalanceRequests.BmrsVolumeRequestAsync(apiKey, urlDate);
-                
-                var returns = JsonSerializer.Serialize(volumes.Select(Mapper.FromXmlToImbalanceVolume));
+            if (apiKey == null)
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
 
-                _logger.LogInformation("Found volumes\n {}", returns);
-            }
-            else 
-                _logger.LogInformation("No volumes found.");
 
+            var volumes = await ImbalanceRequests.BmrsVolumeRequestAsync(apiKey, urlDate);    
+            var returns = JsonSerializer.Serialize(volumes.Select(Mapper.FromXmlToImbalanceVolume));
+
+            return returns.Length > 0 ? 
+                new OkObjectResult(returns) :
+                new NotFoundObjectResult(urlDate);
         }
 
     }
