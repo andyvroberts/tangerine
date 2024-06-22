197671125aa6c2473f1ecc06cd0e16435382adc9
Andy Roberts <avroberts@outlook.com>
2024-06-22 11:53:25 +0100

    month range

--- ingest/Insights/get_system_price.py
+++ ingest/Insights/get_system_price.py
@@ -1,10 +1,22 @@
 import azure.functions as func
+import calendar
 import json
 import logging
 
 blueprint001 = func.Blueprint()
 
-@blueprint001.route(route="prices/{settlementdate}", methods=[func.HttpMethod.GET])
-async def get_system_price(req: func.HttpRequest):
-    sett_date = req.route_params.get('settlementdate')
-    logging.info(f'Get system prices for {sett_date}.')
+@blueprint001.route(route="prices/{year}/{month}", methods=[func.HttpMethod.GET])
+async def get_system_price(req: func.HttpRequest) -> str:
+    _year = req.route_params.get('year')
+    _month = req.route_params.get('month')
+    msg = f'Get system prices for {_year}-{_month}.'
+
+    url = 'https://data.elexon.co.uk/bmrs/api/v1/balancing/settlement/system-prices/'
+
+    mm = int(_month)
+    yyyy = int(_year)
+
+    for dd in range (1, calendar.monthrange(yyyy, mm)[1]):
+        print(f'{url}{yyyy}-{mm:02d}-{dd:02d}')
+
+    return msg
